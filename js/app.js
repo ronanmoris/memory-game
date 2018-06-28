a56b8a488baaedd116dc7848488206ca5f14081d
ronanmoris <ronanmoris@gmail.com>
2018-06-28 15:56:08 +0200

    fix timer

--- js/app.js
+++ js/app.js
@@ -2,17 +2,28 @@
  * Create a list that holds all of your cards
  */
 
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll("li.card");
 const listOfCards = Array.from(cards);
 const deck = document.querySelector(".deck");
 const restartButton = document.querySelector(".restart");
 const secondStar = document.querySelector("ul.stars .second-star");
 const thirdStar = document.querySelector("ul.stars .third-star");
-console.log(secondStar);
+const modal = document.querySelector(".modal");
+const p = document.querySelector(".modal p");
+const playAgainButton = document.querySelector(".modal button");
+const stopWatch = document.querySelector(".stop-watch");
 let moves = document.querySelector(".moves");
 let selectedCard = undefined;
 let solving = false;
 let counter = 0;
+let stars = 3;
+//Variables for timer
+let clearTime;
+let seconds = 0,
+    minutes = 0,
+    hours = 0;
+let secs, mins, gethours;
+let invokeStartWatch = true;
 
 /*
  * Display the cards on the page
@@ -73,14 +84,20 @@ function isMatch(cardA, cardB) {
     return result;
 }
 
-function applyMatch(arrCards) {
-    arrCards.forEach(card => {
+function applyMatch(cardsArr) {
+    cardsArr.forEach(card => {
         card.classList.add("match");
     });
 }
 
-function hideCard(arrCards) {
-    arrCards.forEach(card => {
+// function applyNoMatch(cardsArr) {
+//     cardsArr.forEach(card => {
+//         card.classList.add("nomatch");
+//     });
+// }
+function hideCard(cardsArr) {
+    cardsArr.forEach(card => {
+        // card.classList.add("nomatch");
         card.classList.remove("show", "open");
     });
 }
@@ -88,7 +105,9 @@ function hideCard(arrCards) {
 function incrementMovesCounter() {
     counter += 1;
     if (counter % 2 === 0) {
-        moves.innerText = counter / 2;
+        counter === 2
+            ? (moves.innerHTML = counter / 2 + " Move")
+            : (moves.innerHTML = counter / 2 + " Moves");
     }
 }
 
@@ -96,48 +115,82 @@ function checkAllCardsMatched() {
     return document.querySelectorAll(".match").length === 16;
 }
 
-restartButton.addEventListener("click", () => {
+function restart() {
     window.location.reload(true);
+}
+
+restartButton.addEventListener("click", () => {
+    restart();
 });
 
-function milisecondToMinute(ms) {
-    let gameTime;
-    // 1- Convert to seconds:
-    let seconds = ms / 1000;
-    // 2- Extract hours:
-    let hours = parseInt(seconds / 3600); // 3,600 seconds in 1 hour
-    seconds = seconds % 3600; // seconds remaining after extracting hours
-    // 3- Extract minutes:
-    let minutes = parseInt(seconds / 60); // 60 seconds in 1 minute
-    // 4- Keep only seconds not extracted to minutes:
-    seconds = seconds % 60;
-    gameTime = `${minutes} minute(s) and ${Math.floor(seconds)} second(s)`;
-    console.log("game time: ", gameTime);
+// function milisecondToMinute(ms) {
+//     let gameTime;
+//     // Convert to seconds:
+//     let seconds = ms / 1000;
+//     // Extract minutes:
+//     let minutes = parseInt(seconds / 60); // 60 seconds in 1 minute
+//     // Keep only seconds not extracted to minutes:
+//     seconds = seconds % 60;
+//     gameTime = `${minutes} minute(s) and ${Math.floor(seconds)} second(s)`;
+//     return gameTime;
+// }
+function startWatch() {
+    if (seconds === 60) {
+        seconds = 0;
+        minutes += 1;
+    }
+    mins = minutes < 10 ? "0" + minutes + ": " : minutes + ": ";
+    if (minutes === 60) {
+        minutes = 0;
+        hours += 1;
+    }
+    gethours = hours < 10 ? "0" + hours + ": " : hours + ": ";
+    secs = seconds < 10 ? "0" + seconds : seconds;
+    stopWatch.innerHTML = "Time: " + gethours + mins + secs;
+    seconds++;
+    clearTime = setTimeout("startWatch()", 1000);
 }
 
 function displayRating(counter) {
-    console.log("counter inside function ", counter);
-    if (counter > 5) {
+    if (counter >= 26) {
         thirdStar.classList.add("far");
+        stars = 2;
     }
-    if (counter > 10) {
+    if (counter >= 32) {
         secondStar.classList.add("far");
+        stars = 1;
     }
 }
 
+function displayModal(time) {
+    modal.classList.toggle("open");
+    p.innerHTML = `In ${time} with ${counter / 2} moves and ${stars} star(s)`;
+}
+
+playAgainButton.addEventListener("click", () => {
+    restart();
+});
+
 function selectCard() {
-    let beginTime = performance.now();
+    // let beginTime = performance.now();
+
     const card = document.querySelector("ul.deck");
 
     card.addEventListener("click", e => {
+        if (invokeStartWatch) {
+            startWatch();
+        }
+        invokeStartWatch = false;
+
         if (selectCard && solving) {
             return;
         }
+        let match = false;
+        let clickedCard = e.target;
+
         incrementMovesCounter();
         displayRating(counter);
         displayCardsSymbol(e.target);
-        let match = false;
-        let clickedCard = e.target;
 
         if (selectedCard) {
             match = isMatch(clickedCard, selectedCard);
@@ -145,15 +198,17 @@ function selectCard() {
                 applyMatch([clickedCard, selectedCard]);
                 selectedCard = undefined;
                 if (checkAllCardsMatched()) {
-                    let endTime = performance.now();
-                    milisecondToMinute(beginTime - endTime);
+                    // let endTime = performance.now();
+                    // let totalTime = milisecondToMinute(beginTime - endTime);
+                    // displayModal(totalTime);
+                    clearTimeout(clearTime);
                 }
-
                 return;
             }
 
             solving = true;
             setTimeout(() => {
+                // applyNoMatch([clickedCard, selectedCard]);
                 hideCard([clickedCard, selectedCard]);
                 selectedCard = undefined;
                 solving = false;
