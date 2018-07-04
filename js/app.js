8af0ccaac0e4e5ba7c7289905a792b5b7bb1c96b
ronanmoris <ronanmoris@gmail.com>
2018-07-04 12:08:44 +0200

    add comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 const cards = document.querySelectorAll("li.card");
+const card = document.querySelector("ul.deck");
 const listOfCards = Array.from(cards);
 const deck = document.querySelector(".deck");
-const restartButton = document.querySelector(".restart");
 const secondStar = document.querySelector("ul.stars .second-star");
 const thirdStar = document.querySelector("ul.stars .third-star");
+const restartButton = document.querySelector(".restart");
+const stopWatch = document.querySelector(".stop-watch");
+
+/** Variables for modal */
 const modal = document.querySelector(".modal");
 const p = document.querySelector(".modal p");
 const playAgainButton = document.querySelector(".modal button");
-const stopWatch = document.querySelector(".stop-watch");
+
 let moves = document.querySelector(".moves");
 let counter = 0;
 let stars = 3;
-//Variables for timer
+
+/** Variables for timer */
 let secs, mins;
 let clearTime;
 let seconds = 0,
     minutes = 0;
 
-const shuffledCards = shuffle(listOfCards);
-
-shuffledCards.forEach(card => {
-    deck.appendChild(card);
-});
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+/** Shuffle function from http://stackoverflow.com/a/2450976 */
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue,
@@ -102,21 +101,27 @@ restartButton.addEventListener("click", () => {
     restart();
 });
 
+/**  create timer to calculate game time */
 function startWatch() {
+    /** add minute */
     if (seconds === 60) {
         seconds = 0;
         minutes += 1;
     }
     mins = minutes + " mins ";
-    if (minutes === 60) {
-        minutes = 0;
-    }
     secs = seconds + " secs";
+    /** add timer to html */
     stopWatch.innerHTML = mins + secs;
+    /** add seconds */
     seconds++;
+    /** call startWatch every second */
     clearTime = setTimeout("startWatch()", 1000);
 }
 
+/**
+ * @description uses counter to change class attribute and count stars
+ * @param {number} counter
+ */
 function displayRating(counter) {
     if (counter >= 26) {
         thirdStar.classList.add("far");
@@ -128,33 +133,44 @@ function displayRating(counter) {
     }
 }
 
+/**
+ * @description display modal after player wins the game
+ * @param {string} time
+ */
 function displayModal(time) {
     modal.classList.toggle("open");
     p.innerHTML = `In ${time} with ${counter / 2} moves and ${stars} star(s)!`;
 }
 
 function selectCard() {
-    const card = document.querySelector("ul.deck");
+    /** set boolean to fire startWatch only once */
     let invokeStartWatch = true;
+    /** set boolean to allow click only */
     let solving = false;
-    let selectedCard = undefined;
+    let openCard = undefined;
+    /** shuffle cards */
+    const shuffledCards = shuffle(listOfCards);
+    shuffledCards.forEach(card => {
+        deck.appendChild(card);
+    });
 
+    /** listen for click on card deck */
     card.addEventListener("click", e => {
-        //only listen for click on li element
-        if (e.target && e.target.nodeName === "LI") {
+        let clickedCard = e.target;
+        /** only listen for click on li element */
+        if (clickedCard && clickedCard.nodeName === "LI") {
             let match = false;
-            let clickedCard = e.target;
-            //using invokeStartWatch to fire startWatch only once
+
+            /** using invokeStartWatch to fire startWatch only once */
             if (invokeStartWatch) {
                 startWatch();
             }
-
             invokeStartWatch = false;
-
-            if (selectCard && solving) {
+            /** block clicking on more than two cards */
+            if (solving) {
                 return;
             }
-            //avoiding two clicks on the same card
+            /** avoiding two clicks on the same card */
             if (isOpen(clickedCard)) {
                 return;
             }
@@ -163,30 +179,37 @@ function selectCard() {
             incrementMovesCounter();
             displayRating(counter);
 
-            if (selectedCard) {
-                match = isMatch(clickedCard, selectedCard);
+            /** run only if one card is already open */
+            if (openCard) {
+                match = isMatch(clickedCard, openCard);
                 if (match) {
-                    applyMatch([clickedCard, selectedCard]);
-                    selectedCard = undefined;
+                    applyMatch([clickedCard, openCard]);
+                    openCard = undefined;
                     if (checkAllCardsMatched()) {
-                        displayModal(stopWatch.innerHTML);
+                        /** display modal only after some time so the player can see the entire deck matched */
+                        setTimeout(() => {
+                            displayModal(stopWatch.innerHTML);
+                        }, 1300);
+
                         clearTimeout(clearTime);
                     }
                     return;
                 }
-                applyNoMatch([clickedCard, selectedCard]);
+                applyNoMatch([clickedCard, openCard]);
                 solving = true;
+                /** hide cards after 1.3 seconds and reset openCard and solving */
                 setTimeout(() => {
-                    hideCard([clickedCard, selectedCard]);
-                    selectedCard = undefined;
+                    hideCard([clickedCard, openCard]);
+                    openCard = undefined;
                     solving = false;
-                }, 1500);
+                }, 1300);
                 return;
             }
-            selectedCard = e.target;
+            openCard = e.target;
         }
     });
 }
+/** restart the game */
 playAgainButton.addEventListener("click", () => {
     restart();
 });
